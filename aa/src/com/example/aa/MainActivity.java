a7a472a6d29a34188e68319f10b17ca2d95ff928
wenfeng_chen <wenfeng_chen@kingdee.com>
2014-09-09 17:26:53 +0800

    hotfix2

--- aa/src/com/example/aa/MainActivity.java
+++ aa/src/com/example/aa/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 /**
-*add to test by master
+*add to test by hotfix2
 *
 **/
 public class MainActivity extends ActionBarActivity {
