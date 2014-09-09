f690378964ec5f2eb92f7dbe7f101a9b39fc5a12
wenfeng_chen <wenfeng_chen@kingdee.com>
2014-09-09 16:55:27 +0800

    master 注释

--- aa/src/com/example/aa/MainActivity.java
+++ aa/src/com/example/aa/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 /**
-*add to test
+*add to test by master
 *
 **/
 public class MainActivity extends ActionBarActivity {
