cfc9fbf6a25ad6b7cbbd4757f6b1de85b6b51d70
aopi1125 <aopi1125@sina.com>
2014-09-10 17:17:26 +0800

    Github web updated
    test in Web

--- aa/src/com/example/aa/MainActivity.java
+++ aa/src/com/example/aa/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 /**
-*add to test by master
+*add to test by aopi1125 in Gighut Web 20140910
 *
 **/
 public class MainActivity extends ActionBarActivity {
